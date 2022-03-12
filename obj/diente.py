f0e5d060b4d05d60715e7cb2beaa929a2a84fceb
Alejandro Botero <boterof.alejandro@gmail.com>
2022-03-12 13:47:47 -0500

    Se agrega función de cálculo de NI en la clase Diente

--- obj/diente.py
+++ obj/diente.py
@@ -5,6 +5,11 @@ class Diente:
 
     _atributos = ('Normal', 'Ausente', 'Extruido', 'Intruido')
 
+    diente = 0
+    atributos = ''
+    titulos = ()
+    valores = {}
+
     def get_titulos(diente):
         '''Devuelve el tuple correspondiente si es Superior o Inferior'''
         superior = diente <= 28 or (diente >= 51 and diente <= 68)
@@ -21,14 +26,21 @@ class Diente:
         '''Crea un diccionario con los datos del diente correspondiente'''
         res = {}
         for val in titulos:
-            #Inicializa cada valor en el diccionario según si usa 3 números, 1 número o guión
-            if re.search(r'L\.M\.G|SONDAJE|MARGEN|SUPURA|SANGRA', val):
-                res[val] = (0,0,0)
-            elif re.search(r'N\.I', val):
-                res[val] = 0
-            else:
-                res[val] = '-'
+            res[val] = None
         return res
+
+    def calcular_ni(self, opt = ''):
+        '''Si se han especificado margen y sondaje, calcula el nivel de inserción
+            Si se especifica opt = '_' usa los datos de _MARGEN y _SONDAJE en _N.I.'''
+
+        # Si no ha especificado Margen #,#,# y Sondaje #,#,# deja vacío el Nivel de inserción
+        if self.valores[opt+'MARGEN'] is None or self.valores[opt+'SONDAJE'] is None:
+            self.valores[opt+'N.I.'] = None
+            return
+        res = []
+        for i in range(3):
+            res.append(self.valores[opt+'MARGEN'][i] - self.valores[opt+'SONDAJE'][i])
+        self.valores[opt+'N.I.'] = tuple(res)
 
     def __init__(self, diente):
         '''Inicializa el diente'''
@@ -37,9 +49,14 @@ class Diente:
         self.titulos = Diente.get_titulos(diente)
         self.valores = Diente.init_valores(diente, self.titulos)
 
-        # Muestra el diccionario inicializado, para efectos de prueba, esto se quita después
-        print(self.valores)
-
 # Inicializar objetos... Esto se debe hacer desde la función externa cuando esté implementado
 diente_11 = Diente(11)
-diente_11 = Diente(34)
+diente_11.valores['MARGEN'] = -2, -1, -3
+diente_11.valores['SONDAJE'] = 4, 1, 0
+diente_11.calcular_ni()
+print(diente_11.valores)
+print('------------------')
+diente_34 = Diente(34)
+diente_34.calcular_ni(opt = '_')
+print(diente_34.valores)
+print(diente_34.valores['N.I.'])
